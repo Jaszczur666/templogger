c870a96faa33f923c111ac898caf9b41f257875d
Jarosław Komar <j.komar@intibs.pl>
2021-01-29 10:18:10 +0100

    Initial commit

---
+++ TempLogger/TempLogger/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TempLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea4.AxisX.IsStartedFromZero = false;
+            chartArea4.AxisY.IsStartedFromZero = false;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Location = new System.Drawing.Point(142, 23);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(646, 393);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.Chart1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 375);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Zapisz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ TempLogger/TempLogger/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TempLogger
+{
+    public partial class Form1 : Form
+    {
+        static Stopwatch sw=new Stopwatch();
+        public double temperature;
+        private long i=0;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.Info("Start programu");
+            sw.Start();
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (string s in ports)
+            {
+               comboBox1.Items.Add(s);
+                
+            }
+            try
+            {
+                string config = System.IO.File.ReadAllText("ports.cfg");
+                var result = System.Text.RegularExpressions.Regex.Split(config, "\r\n|\r|\n");
+                comboBox1.Text = result[0];
+            }
+            catch { };
+            //            serialPort1.Open();
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateChart()
+        {
+            Invoke((MethodInvoker)delegate {
+                log.Info("Chart update ");
+                log.Debug("temperature is " + temperature.ToString()) ;
+            chart1.Series[0].Points.AddXY(sw.Elapsed.TotalSeconds, temperature);
+            });
+
+
+        }
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadLine();
+            System.Globalization.CultureInfo cul = System.Globalization.CultureInfo.InvariantCulture;
+            double.TryParse(data,System.Globalization.NumberStyles.Float,System.Globalization.CultureInfo.InvariantCulture, out temperature);
+            log.Debug("dane "+ sw.Elapsed.TotalSeconds.ToString("g5",cul)+" "+temperature.ToString("g5", cul));
+            System.IO.File.AppendAllText("zrzut.dat", sw.Elapsed.TotalSeconds.ToString("g5", cul) + " " + temperature.ToString("g5", cul) +" "+ DateTime.Now.ToString() + "\r\n") ;
+
+            if (i % 20 == 0)
+            {
+                UpdateChart();
+                i = 0;
+            }
+            i++;
+            log.Debug("i=" + i.ToString());
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            log.Debug("Startujmy");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string config;
+            config = comboBox1.Text;
+            System.IO.File.WriteAllText("ports.cfg", config);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                string wynik="";
+                int size = chart1.Series[0].Points.Count;
+                for (int i = 0; i < size; i++) {
+                    wynik += chart1.Series[0].Points[i].XValue.ToString() + " " + chart1.Series[0].Points[i].YValues[0].ToString()+"\r\n";
+                        }
+                File.WriteAllText(saveFileDialog1.FileName, wynik);
+            }
+        }
+    }
+}
